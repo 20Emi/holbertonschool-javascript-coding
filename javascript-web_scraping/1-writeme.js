609149f0d7676966474d33f5eb928168b873d84f
20Emi <iop.emilysanchez1@gmail.com>
2023-09-18 08:57:11 -0700

    task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const file = process.argv[2];
+const contenuto = process.argv[3];
+
+fs.writeFile(file, contenuto, 'utf-8', (error) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+});
