867050c388e38b522ffdf385d05b997d147d6be1
20Emi <iop.emilysanchez1@gmail.com>
2023-09-18 08:45:48 -0700

    update semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,13 +2,12 @@
 
 const fs = require('fs');
 
-
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (error, data) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log(data)
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(data);
 });
