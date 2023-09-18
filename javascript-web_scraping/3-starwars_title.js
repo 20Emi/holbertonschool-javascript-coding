e58af6c702b1a6f2d7af4652998df874e22b38d9
20Emi <iop.emilysanchez1@gmail.com>
2023-09-18 10:10:40 -0700

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,12 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/'
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
 request(url + id, function (error, response, body) {
   if (error) {
     console.error(error);
-  }
-  else {
+  } else {
     const data = JSON.parse(body);
     console.log(data.title);
   }
