5be6a4e271efa4b5eb299a6210b8f8a8e127a1bd
20Emi <iop.emilysanchez1@gmail.com>
2023-09-18 08:58:32 -0700

    update semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 const file = process.argv[2];
 const contenuto = process.argv[3];
 
 fs.writeFile(file, contenuto, 'utf-8', (error) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
+  if (error) {
+    console.error(error);
+  }
 });
