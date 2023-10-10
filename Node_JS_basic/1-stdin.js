50ba2f9860aa3758db6bf90d38c3e94809ff7e7d
20Emi <iop.emilysanchez1@gmail.com>
2023-10-09 17:22:39 -0700

    task 1..

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
+  process.stdout.write(`Your name is: ${data}`);
   process.exit(console.log('This important software is now closing\n'));
 });
